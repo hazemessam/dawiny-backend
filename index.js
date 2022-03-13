2fa33d33497cbe9c961d60b2975243924657cccd
Hazem Essam <hzmessam.dev@outlook.com>
2022-03-13 13:22:11 +0200

    Add database support

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+// Third party modules
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Application modules
 const app = require('./app');
 
 
-const port = process.env.PORT || 8080;
+dotenv.config();
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-});
+const PORT = process.env.PORT || 8080;
+const DB_URI = process.env.DB_URI;
+
+
+const runserver = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log(`Connected to db ${mongoose.connection.db.databaseName}`);
+        app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+    } catch (error) {
+        console.error(error);
+        console.error('Can not connect to the db!');
+    }
+}
+
+
+runserver();
